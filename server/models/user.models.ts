833e185239136d9e1e596c0545d500db2893c314
abdel rahman Hassan <st20182841@gmail.com>
2024-04-29 20:36:00 +0300

    formated user model

--- server/models/user.models.ts
+++ server/models/user.models.ts
@@ -1,90 +1,84 @@
-import { model, Schema ,Document, Model} from "mongoose";
+import { model, Schema, Document, Model } from "mongoose";
 import mongoose from "mongoose";
-import bycrypt from 'bcryptjs';
+import bycrypt from "bcryptjs";
 
-const emailRegexPattern :RegExp = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/
+const emailRegexPattern: RegExp = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/;
 
-export interface IUser extends Document
-{
-    name:string,
-    email:string,
-    password:string,
-    avatar:
-    {
-        public_id:string,
-        url:string
-    },
-    role:string,
-    isVerified:boolean,
-    courses:Array<{courseId : string}>;
-    comparePassword: (password : string) => Promise<boolean>;
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+  role: string;
+  isVerified: boolean;
+  courses: Array<{ courseId: string }>;
+  comparePassword: (password: string) => Promise<boolean>;
 }
 
-const userSchema : Schema<IUser> = new mongoose.Schema({
-    name:
-    {
-        type:String,
-        required:[true,"please enter your name"]
+const userSchema: Schema<IUser> = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please enter your name"],
     },
-    email:
-    {
-        type:String,
-        required:[true,"please enter your email"],
-        validate:{
-            validator: function(value:string)
-            {
-                return emailRegexPattern.test(value);
-            },
-            message:"enter a valid email",
+    email: {
+      type: String,
+      required: [true, "please enter your email"],
+      validate: {
+        validator: function (value: string) {
+          return emailRegexPattern.test(value);
         },
-        unique:true,
+        message: "enter a valid email",
+      },
+      unique: true,
     },
-    password:
-    {
-        type:String,
-        required:[true,"please enter your password"],
-        minlength:[6,"password must be atleast 6 characters long"],
-        select:false
+    password: {
+      type: String,
+      required: [true, "please enter your password"],
+      minlength: [6, "password must be atleast 6 characters long"],
+      select: false,
     },
-    avatar:
-    {
-        public_id:String,
-        url:String,
+    avatar: {
+      public_id: String,
+      url: String,
     },
-    role:
-    {
-        type:String,
-        default:"user"
+    role: {
+      type: String,
+      default: "user",
     },
-    isVerified:
-    {
-        type:Boolean,
-        default:false,
+    isVerified: {
+      type: Boolean,
+      default: false,
     },
-    courses:[
-    {
-        courseId:String,
-    }]
-},{timestamps:true});
+    courses: [
+      {
+        courseId: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-// hash password 
+// hash password
 
-userSchema.pre<IUser>("save", async function(next)
-{
-    if (!this.isModified("password"))
-        {
-            next();
-        }
-        this.password = await bycrypt.hash(this.password,10);
-        next();
+userSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bycrypt.hash(this.password, 10);
+  next();
 });
 
 // compare promise
 
-userSchema.methods.comparePassword = async function(enteredPassword : string): Promise<boolean>
-{
-    return await bycrypt.compare(enteredPassword,this.password);
+userSchema.methods.comparePassword = async function (
+  enteredPassword: string
+): Promise<boolean> {
+  return await bycrypt.compare(enteredPassword, this.password);
 };
-const userModel : Model<IUser> = mongoose.model("User",userSchema); 
+const userModel: Model<IUser> = mongoose.model("User", userSchema);
 
 export default userModel;
