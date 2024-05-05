09cbda96ed925b25288d4a5e0c75f4df696855e0
abdel rahman Hassan <st20182841@gmail.com>
2024-05-05 20:15:43 +0300

    added update password

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,6 +1,6 @@
 import { Response, Request, NextFunction } from 'express';
 import express from 'express';
-import { activateUser, LoginUser, registrationUser , logoutUser, updateAccessToken, getUserInfo, socialAuth, updateUserInfo } from '../controllers/user.controller';
+import { activateUser, LoginUser, registrationUser , logoutUser, updateAccessToken, getUserInfo, socialAuth, updateUserInfo, updatePassword } from '../controllers/user.controller';
 import { authorizeRole, isAuth } from '../middleware/auth';
 const userRouter = express.Router();
 
@@ -19,5 +19,8 @@ userRouter.get("/me",isAuth,getUserInfo);
 userRouter.post("/social-auth",socialAuth);
 
 userRouter.put("/update-user-info",isAuth,updateUserInfo);
+
+userRouter.put("/updatePassword",isAuth,updatePassword);
+
 
 export default userRouter;
